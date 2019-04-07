7e8f9d47fc8f454d16392e0e038576b1c4c522dc
Hameed Abdulrahaman <hameedabdulrahamann@gmail.com>
2019-04-07 01:26:42 -0700

    feature(account-rest-endpoints):add rest account endpoints [Finishes #165165555]

---
+++ resource/api/model/account.js
@@ -0,0 +1,40 @@
+const accounts = [
+  {
+    id: 1,
+    accountNumber: 5378367657,
+    createdOn: '2019-06-06T21:43:27.124Z',
+    owner: 3,
+    type: 'savings',
+    status: 'active',
+    balance: 81000.00,
+  },
+  {
+    id: 2,
+    accountNumber: 6868767657,
+    createdOn: '2019-01-06T21:43:27.124Z',
+    owner: 1,
+    type: 'savings',
+    status: 'active',
+    balance: 15000.00,
+  },
+  {
+    id: 3,
+    accountNumber: 58897676867,
+    createdOn: '2019-05-06T21:43:27.124Z',
+    owner: 4,
+    type: 'savings',
+    status: 'active',
+    balance: 10900.00,
+  },
+  {
+    id: 4,
+    accountNumber: 77865765880,
+    createdOn: '2015-04-06T21:43:27.124Z',
+    owner: 2,
+    type: 'savings',
+    status: 'active',
+    balance: 6000.00,
+  },
+];
+
+module.exports = accounts;
