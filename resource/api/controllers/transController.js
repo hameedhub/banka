0e98ec87f915775f85efe51a1c509f85c99113c1
Hameed Abdulrahaman <hameedabdulrahamann@gmail.com>
2019-04-11 11:09:02 -0700

    feature(endpoint-credit-account):add credit account endpoint [Finishes #165262964]

--- resource/api/controllers/transController.js
+++ resource/api/controllers/transController.js
@@ -48,6 +48,43 @@ class TransController {
     });
   }
 
+  static creditTrans(req, res) {
+    const account = accounts.find((acc => acc.accountNumber === parseInt(req.params.accountNumber)));
+    if (!account) {
+      return res.status(404).json({
+        status: 404,
+        error: 'Account not found',
+      })
+      ; }
+    const val = validate.trans(req.body);
+    if(val.error) { return res.status(400).json({
+      status: 400,
+      error: val.error.details[0].message
+    });
+    }
+    const accountBal = parseFloat(account.balance) + parseFloat(req.body.amount) ;
+    transaction.push({
+      id: transaction.length + 1,
+      createdOn: new Date(),
+      type: 'credit',
+      accountNumber: req.params.accountNumber,
+      cashier: req.body.cashier,
+      amount: req.body.account,
+      oldBalance: account.balance,
+      newBalance: accountBal,
+    });
+    return res.status(201).json({
+      status: 201,
+      data: {
+        transactionId: transaction.length + 1,
+        accountNumber: req.params.accountNumber,
+        amount: req.body.amount,
+        cashier: req.body.cashier,
+        transactionType: 'credit',
+        accountBalance: accountBal
+      }
+    });
+  }
 }
 
 export default TransController;
