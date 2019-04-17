2cc8efcbeaf9bb24fec2756112a9b78df6167114
Hameed Abdulrahaman <hameedabdulrahamann@gmail.com>
2019-04-17 12:51:11 -0700

    chore(postgres-setup):add Setup postgres env [Finishes #165427514]

---
+++ api/model/database.js
@@ -0,0 +1,11 @@
+import { pool } from 'pg';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const pool = new Pool({
+    user:process.env.DB_USER,
+    host:process.env.DB_HOST,
+    database:process.env.DB_DATABASE,
+    password:process.env.DB_PASS,
+    port:process.env.DB_PORT
+})
