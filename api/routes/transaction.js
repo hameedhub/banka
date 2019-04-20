9f304027afb54654f0dd03f0fa0b627701708786
Hameed Abdulrahaman <hameedabdulrahamann@gmail.com>
2019-04-19 23:59:00 -0700

    feature(user-specific-trans):add user get specific transaction [Finishes #165414043]

--- api/routes/transaction.js
+++ api/routes/transaction.js
@@ -6,5 +6,6 @@ const router = Router();
 
 router.post('/:accountNumber/debit', Auth.checkToken, Controller.debitTrans);
 router.post('/:accountNumber/credit', Auth.checkToken, Controller.creditTrans);
+router.get('/:id', Auth.checkToken, Controller.getTransById);
 
 export default router;
